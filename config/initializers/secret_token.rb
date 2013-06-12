22e5a0e45a26a8895d8e38d02f214f578cb162c0
Daniel Spangenberg <danielsp@amazon.com>
2013-06-12 13:51:32 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreezingDangerzone::Application.config.secret_token = '1c7ce31f8c22e7236ded6303200008e7c3e7183cfa29fac4881c9cbafb362546945748f1f91a17acc9dbf9120eef071fbb2472fe75cabbd4644561641d87860e'
